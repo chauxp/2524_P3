ed7636e26f35cd6cccb5dd7b27b10a036e61ae11
Pierre Chaux <pierrechaux@localhost.localdomain>
2020-12-06 13:02:49 -0500

    Imported tk library and created basic window

--- DirectoryTree_P3.py
+++ DirectoryTree_P3.py
@@ -4,16 +4,24 @@
 
 
 import os
+import tkinter as tk
 
 def main():
-	directory = '/home/pierrechaux/Documents/'
-	os.chdir(directory)
-	print (dir(os.path))
+	directory = os.environ.get('HOME')
+	#os.chdir(directory)
+	print (os.environ.get('HOME'))
+	window = tk.Tk()
+	#tk._test()
+	#app = tk.Window(window)
+	greeting = tk.Label(text="Hello, Tkinter")
+	greeting.pack()
+	window.mainloop()
 	
 	treeGenerator(directory)
 	print (os.path.basename('/home/pierrechaux/Documents.txt'))
 
 def treeGenerator(pathName):
+	print(pathName)
 	for dirpath, dirnames, filenames in os.walk(pathName):
 		print('dirpath:',dirpath)
 		print('dirname: ',dirnames)
