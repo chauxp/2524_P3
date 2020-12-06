9f26d307dd16e5d8d07f33f525f9345f99675aec
Pierre Chaux <pierrechaux@localhost.localdomain>
2020-12-06 18:10:51 -0500

    Fixed some errors and added some comments on class

--- DirectoryTree_P3.py
+++ DirectoryTree_P3.py
@@ -17,9 +17,11 @@ def main():
 	#print (os.environ.get('HOME'))
 	window = tk.Tk() #create tk window
 	test = DirectoryTreeGUI(window)
-	test.getDatafromRoot()
+	
 	test.getContents()
+	
 	window.mainloop()
+	print(test.getRoot())
 	#tk._test()
 	#app = tk.Window(window)
 	greeting = tk.Label(text="Hello, Tkinter") 
@@ -34,7 +36,7 @@ def treeGenerator(rootName):
 	#print(rootName)
 	#print(os.listdir())
 	test = DirectoryTreeGUI()
-	test.getDatafromRoot()
+	
 	test.getContents()
 	window.mainloop()
 	
@@ -73,45 +75,40 @@ def toggleDirButton(but, selected):
 
 class DirectoryTreeGUI:
 	def __init__ (self, master, rootDirectory= os.environ.get('HOME'), subpaths=[]):
-		myFrame = tk.Frame(master)
+		windowFrame = tk.Frame(master) ##tk master window passed through constructor
 		self.rootDirectory = rootDirectory
 		os.chdir(self.rootDirectory)
 		self.subpaths = subpaths
 		self.master = master
-		#self.getDatafromRoot()
+		self.getDatafromRoot()
 
 		
-	def push(self, x):
-		self.subpaths.append(x)
-		
-	def setRoot(self, rootDir):
-		self.rootDirectory = rootDir
+	def push(self, x): #append to subpaths
+		self.subpaths.append(x)	
 	
-	def getRoot(self):
-		return self.rootDirectory
-		
-	def pop(self):
-		x = self.items[-1]
-		del self.items[-1]
+	def pop(self): ##pop from subpaths
+		x = self.subpaths[-1]
+		del self.subpaths[-1]
 		return x
 		
-	def empty(self):
-		return len(self.items)== 0
-		
-	def selectFolder(self,PathName):
+	def selectFolder(self,PathName): #when a folder is selected, the options change
 		return 0
 	
-	def selectFile(self,PathName):
-		return 0
-	
-	def changeRoot(self, RootName):
+	def selectFile(self,PathName): #when a file is selected, the options change
 		return 0
 		
-	def getContents(self):
+	def getContents(self): #returns contents of array -for testing
 		print(self.subpaths)
 		print(len(self.subpaths))
+		
+	def changeRoot(self,newRoot): #set new root
+		self.rootDirectory = newRoot
+		os.chdir(self.rootDirectory)
+		
+	def getRoot(self): #gets root name
+		return self.rootDirectory
 	
-	def getDatafromRoot(self):
+	def getDatafromRoot(self): #stores all subdirectories in subpaths list, with the name in the first index of the list, followed by (if they are a directory) their own subdirectories
 		rootName = self.rootDirectory
 		##treeGenerator(rootName):
 		#print(rootName)
@@ -123,24 +120,28 @@ class DirectoryTreeGUI:
 			
 			
 			if os.path.isdir(pathName): ##IF IT IS A DIRECTORY
-				print('THIS IS A DIRECTORY: ', path)
+				#print('THIS IS A DIRECTORY: ', path)
 				subdirs.append(pathName)
 				for subpath in os.listdir():
 					subName = os.path.join(pathName,subpath)
 					subdirs.append(subName)
 										
-				print('THESE ARE THE SUBDIRECTORIES: ')
-				print(os.listdir(pathName))
+				#print('THESE ARE THE SUBDIRECTORIES: ')
+				#print(os.listdir(pathName))
 				#self.push(subdirs)
 					
 			elif os.path.isfile(pathName): ##IF IT IS A FILE
-				print('THIS IS A FILE: ', path)
+				#print('THIS IS A FILE: ', path)
 				subdirs.append(pathName)
 					
 			else: ##IF IT IS NEITHER
 				print('THIS IS NOTHING: ', pathName)
 		
 			self.push(subdirs)
+			#os.chdir(self.rootDirectory)
+			
+	def makeGUI(self):
+		return 0
 		
 	
 		
